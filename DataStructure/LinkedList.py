7ed76ed3ae464c5e0c2c10a462e0cc50c9b42547
Ajay Agrawal <ajayagrawal1905@gmail.com>
2020-12-19 01:07:51 +0530

    Linked List Code

---
+++ DataStructure/LinkedList.py
@@ -0,0 +1,13 @@
+class Node:
+    def __init__(self, value=None):
+        self.value = value
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+
+list1 = LinkedList()
+list1.head = Node(10);
+
