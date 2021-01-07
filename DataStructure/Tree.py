980b0c54ddfcad693ae29a792d4f23ad04d66390
Ajay Agrawal <ajayagrawal1905@gmail.com>
2021-01-07 07:30:02 +0530

    Working on Tree.py

---
+++ DataStructure/Tree.py
@@ -0,0 +1,11 @@
+'''
+Author Name : Ajay Manoj Agrawal
+Github Link : https://github.com/ajayagrawal1905
+Date : 05-01-2021 Tuesday
+
+'''
+class TreeNode:
+    def __init__(self,data):
+        self.data = data
+        self.children = []
+        self.parent = None
